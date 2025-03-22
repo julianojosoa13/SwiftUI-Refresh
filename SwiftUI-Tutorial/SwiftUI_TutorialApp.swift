e145a20d1af307d3c1c3570c15de23b60d41c42f
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-03-22 17:39:21 +0300

    Initial Commit

---
+++ SwiftUI-Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Juliano RAHAJAHARIMANGA on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
